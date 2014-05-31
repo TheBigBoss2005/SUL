cb6fe7a8d0ecba28d22e71623b39361cdd219d41
Suguru Akiho <suguru.akiho@gmail.com>
2014-05-31 18:13:05 +0900

    rubocopが怒るので修正したよ

--- spec/controllers/top_controller_spec.rb
+++ spec/controllers/top_controller_spec.rb
@@ -2,10 +2,10 @@ require 'spec_helper'
 
 describe TopController do
 
-  describe "トップページ" do
-    it "はイベント一覧ページと支払いページへのボタンが表示される" do
-       visit root_path
-       expect(page).to have_content('')
+  describe 'トップページ' do
+    it 'はイベント一覧ページと支払いページへのボタンが表示される' do
+      visit root_path
+      expect(page).to have_content('')
     end
   end
 
