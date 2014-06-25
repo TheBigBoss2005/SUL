ea5506a59278f7a0d3c1571603f2b4a511dcd30e
seomasanori <ipewlm@hotmail.co.jp>
2014-06-26 01:07:56 +0900

    イベント新規作成画面に参加者選択リストを追加

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,11 +1,16 @@
 class EventsController < ApplicationController
   def new
     @event = Event.new
+    @users = User.all
   end
 
   def create
     @event = Event.new(event_params)
+    @users = User.all
     if @event.save
+      params[:event][:participant_ids].each do |p_id|
+        @event.participants.create(user_id: p_id.to_i) unless p_id.empty?
+      end
       flash[:success] = "イベント#{@event.name}を作成しました"
       # event一覧ページができたらそっちに遷移する
       # それまではイベント作成ページに戻る
--- spec/requests/event_pages_spec.rb
+++ spec/requests/event_pages_spec.rb
@@ -3,7 +3,13 @@ require 'spec_helper'
 describe 'EventPages' do
 
   describe 'GET /newevent' do
-    before { visit newevent_path }
+    before do
+      User.create(name: 'Alpha')
+      User.create(name: 'Bravo')
+      User.create(name: 'Charlie')
+      @user = User.first
+      visit newevent_path
+    end
     title = '新規イベント作成'
 
     it "は'#{title}'の見出しを表示する" do
@@ -13,10 +19,36 @@ describe 'EventPages' do
     it "はページタイトルに'#{title}'を含む" do
       expect(page).to have_title(title)
     end
+
+    it 'はイベント名入力欄を含む' do
+      expect(page).to have_selector(:xpath, "//input[@id='event_name']")
+    end
+
+    it 'はメモ入力欄を含む' do
+      expect(page).to have_selector(:xpath, "//input[@id='event_memo']")
+    end
+
+    it 'は開催日入力欄を含む' do
+      expect(page).to have_selector(:xpath, "//input[@id='event_date']")
+    end
+
+    it 'は参加者選択欄を含む' do
+      expect(page).to have_selector(:xpath, "//select[@id='event_participant_ids']")
+    end
+
+    it 'は参加者選択肢に登録済ユーザを含む' do
+      expect(page).to have_selector(:xpath, "//option[@value='#{@user.id}']")
+    end
   end
 
   describe 'イベント作成機能' do
-    before { visit newevent_path }
+    before do
+      User.create(name: 'Alpha')
+      User.create(name: 'Bravo')
+      User.create(name: 'Charlie')
+      @user = User.first
+      visit newevent_path
+    end
     let(:submit) { '確定' }
 
     describe '無効な登録内容のとき' do
@@ -42,12 +74,19 @@ describe 'EventPages' do
         fill_in 'Name', with: 'test event'
         fill_in 'Memo', with: 'hoge'
         fill_in 'Date', with: '2014/01/01'
+        select @user.name, from: 'event_participant_ids'
       end
 
       specify 'イベントが追加されること' do
         expect { click_button submit }.to change(Event, :count).by(1)
       end
 
+      specify 'イベント参加者が追加されること' do
+        expect { click_button submit }.to change(Participant, :count).by(1)
+      end
+
+      # 本来はイベント一覧ページに遷移する
+      # イベント一覧ページができるまではイベント作成画面に戻る
       specify 'イベント作成画面に戻ること' do
         expect(page).to have_title('新規イベント作成')
       end
