1f69143c508bf771d8f4480c79ac0a13915825bc
Suguru Akiho <suguru.akiho@gmail.com>
2014-08-12 08:25:41 +0900

    清算id指定の削除を実装。清算idを複数指定してまとめて削除したい場合は別途実装が必要

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   # match '/', to: 'top#index', via: 'get', as: 'root_path'
   root 'top#index'
 
-  resources :events
+  resources :events do
+    resources :payments
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
