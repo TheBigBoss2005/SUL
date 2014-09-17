69030504deaaefce51cae8b54a11b8fa885860b1
seomasanori <ipewlm@hotmail.co.jp>
2014-09-17 21:05:28 +0900

    Finished separating event edit event page

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,10 @@ Rails.application.routes.draw do
   # match '/', to: 'top#index', via: 'get', as: 'root_path'
   root 'top#index'
 
-  resources :payments
-  resources :events
+  resources :payments, only: %w(index destroy)
+  resources :events do
+    resources :items
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
