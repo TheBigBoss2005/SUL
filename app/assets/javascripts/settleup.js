5d5dfe474d83e3d2e21f67d64c1ac666c3ecec35
seomasanori <ipewlm@hotmail.co.jp>
2014-10-09 23:07:16 +0900

    Finished implementing clickable-payments-table

--- app/assets/javascripts/settleup.js
+++ app/assets/javascripts/settleup.js
@@ -2,13 +2,34 @@ $(function() {
   // disable settleup button when page loaded
   $("#settleup").prop('disabled', true);
 
-  // enable/disable settleup button when checkbox clicked
-  $(":checkbox").click(function() {
+  // enable/disable settleup button
+  function toggle_settleup_button() {
     if ($("[name='payment_ids[]']:checked").length > 0) {
       $("#settleup").removeAttr('disabled');
     } else {
       $("#settleup").prop('disabled', true);
     }
+  };
+
+  // toggle checkbox when checkbox clicked
+  $(":checkbox[name='payment_ids[]']").click(function() {
+    if ( $(this).prop('checked') ) {
+      $(this).prop('checked', '');
+    } else {
+      $(this).prop('checked', 'checked');
+    }
+    toggle_settleup_button();
+  });
+
+  // toggle checkbox when table row clicked
+  $("table tr").click(function() {
+    var checkbox = $(this).children("td").children(":checkbox");
+    if ( checkbox.prop('checked') ) {
+      checkbox.prop('checked', '');
+    } else {
+      checkbox.prop('checked', 'checked');
+    }
+    toggle_settleup_button();
   });
 });
 
