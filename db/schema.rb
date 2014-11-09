e5b16f14a7f668d73a7a00d8fcf44a6ce4bfab22
Suguru Akiho <suguru.akiho@gmail.com>
2014-11-09 23:10:17 +0900

    event一覧画面の未入力時のデフォルト表示を追加

--- db/schema.rb
+++ db/schema.rb
@@ -60,13 +60,11 @@ ActiveRecord::Schema.define(version: 20141004003959) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.string   "username"
     t.string   "login_id"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["login_id"], name: "index_users_on_login_id", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  add_index "users", ["username"], name: "index_users_on_username", unique: true
 
 end
