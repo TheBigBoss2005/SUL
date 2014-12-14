a4c58cc72160368b4edbd558d447efb067fac7b5
seomasanori <ipewlm@hotmail.co.jp>
2014-12-14 23:07:45 +0900

    支払金額入力欄に割り勘金額がデフォルトで入力されるように変更

--- app/assets/javascripts/itemform.js
+++ app/assets/javascripts/itemform.js
@@ -10,14 +10,34 @@ $(function() {
     });
   });
 
+  // autoinput individually form
+  $("#item_price").change(function() {
+    if ($(this).val() != "") {
+      var price = parseInt($(this).val());
+      var per = $("#source_user_ids :selected").length;
+      if (per != 0 && price != 0) {
+        var individually = parseInt(price / per);
+        $("#source_user_ids :selected").each(function() {
+          $("#payment_price_"+$(this).val()).val(individually);
+        });
+        $("#total").text(getTotal());
+      };
+    };
+  });
+
   // sum individually form values
   $("[id^=payment_price]").change(function() {
+    $("#total").text(getTotal());
+  });
+
+  // get total
+  function getTotal() {
     var total = 0;
     $("[id^=payment_price]").each(function() {
       var num = $(this).val();
       total = num != "" ? total + parseInt(num) : total;
     });
-    $("#total").text(total);
-  });
+    return total;
+  };
 });
 
